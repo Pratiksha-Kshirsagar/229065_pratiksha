678c1787df73523d1379434e3578cfddb24896ff
supriya <supriyasanjay@gmail.com>
2023-01-02 17:15:43 +0530

    copydata

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+var staticFolder=express.static(path.join(__dirname,"public"));
+app.use(staticFolder);
+app.get('/',function(req,res){
+  
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+}) 
+app.listen(8888);
+console.log("server is running");
